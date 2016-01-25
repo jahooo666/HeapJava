4c088add3d7599e32261ff0488cba65d7798c6ef
jahooo666 <okraskaj@ee.pw.edu.pl>
2016-01-26 00:39:45 +0100

    >zaimplementowany heapDown

--- src/Heap.java
+++ src/Heap.java
@@ -1,13 +1,13 @@
-public class Heap{
+public class Heap {
     private int[] data;
     private int heapSize;
 
-    public Heap(int size){
+    public Heap(int size) {
         data = new int[size];
         heapSize = 0;
     }
 
-    public int getMax(){
+    public int getMax() {
         //otrzymywanie najwyzszego elementu kopca
         if (isEmpty())
             throw new HeapException("Heap is empty");
@@ -15,26 +15,26 @@ public class Heap{
             return data[0];
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return (heapSize == 0);
     }
 
-    private int getLeftChildIndex(int nodeIndex){
-        //uzyskiwanie ideksu lewego dziecka- wiêkszego
+    private int getLeftChildIndex(int nodeIndex) {
+        //uzyskiwanie ideksu lewego dziecka- wiï¿½kszego
         return 2 * nodeIndex + 1;
     }
 
-    private int getRightChildIndex(int nodeIndex){
+    private int getRightChildIndex(int nodeIndex) {
         //usyskiwanie indeku prawego dziecka- mniejszego
         return 2 * nodeIndex + 2;
     }
 
-    private int getParentIndex(int nodeIndex){
+    private int getParentIndex(int nodeIndex) {
         //uzyskiwanie indeksu rodzica
         return (nodeIndex - 1) / 2;
     }
 
-    public class HeapException extends RuntimeException{
+    public class HeapException extends RuntimeException {
         public HeapException(String message) {
             super(message);
         }
@@ -43,32 +43,48 @@ public class Heap{
     public void insert(int value) {
         heapSize++;
         data[heapSize - 1] = value;
-        heapUp(heapSize-1);
+        heapUp(heapSize - 1);
     }
 
-    public void heapUp(int n){
-        int i = n-1;
-        while(i>0){
+    public void heapUp(int n) {
+        int i = n - 1;
+        while (i > 0) {
             int parent = getParentIndex(i);
-            if(data[parent] >= data[i])
+            if (data[parent] >= data[i])
                 return;
-            swap(i,parent);
-            i=parent;
+            swap(i, parent);
+            i = parent;
         }
     }
 
-    private void swap(int a, int b){
+    public void heapDown(int n) {
+        int act = 0;
+        int child = getLeftChildIndex(act);
+
+
+        while (child < n) {
+            if (child + 1 < n && data[child + 1] > data[child])
+                child++;
+            if (data[act] >= data[child])
+                return;
+            swap(act,child);
+            act = child;
+            child = getLeftChildIndex(act);
+        }
+    }
+
+
+    private void swap(int a, int b) {
         int temp = data[a];
-        data[a]=data[b];
-        data[b]=temp;
+        data[a] = data[b];
+        data[b] = temp;
     }
 
-    public void print(){
-        for(int i = 0; i < data.length; i++) {
-            System.out.printf("%d-",data[i]);
+    public void print() {
+        for (int i = 0; i < data.length; i++) {
+            System.out.printf("%d-", data[i]);
         }
     }
-
 
 
 }
